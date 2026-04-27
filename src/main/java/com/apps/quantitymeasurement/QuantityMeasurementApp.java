40e28f4621bed5af4a3ba9e7bc5538b73bd6ab79
Mutthuram03 <srmutthuram2007@gmail.com>
2026-04-27 09:32:37 +0530

    UC4: Added Yards and Centimeters using generic Length class

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -4,10 +4,12 @@ import java.util.Objects;
 
 public class QuantityMeasurementApp {
 
-    // Enum for units
+    // Enum for all supported units
     public enum LengthUnit {
         FEET(1.0),
-        INCHES(1.0 / 12.0);
+        INCHES(1.0 / 12.0),
+        YARDS(3.0),
+        CENTIMETERS(0.0328084);
 
         private final double toFeet;
 
@@ -26,10 +28,14 @@ public class QuantityMeasurementApp {
         private final LengthUnit unit;
 
         public Length(double value, LengthUnit unit) {
+            if (unit == null) {
+                throw new IllegalArgumentException("Unit cannot be null");
+            }
             this.value = value;
             this.unit = unit;
         }
 
+        // Convert to base unit (Feet)
         private double toBase() {
             return unit.toFeet(value);
         }
@@ -50,10 +56,20 @@ public class QuantityMeasurementApp {
         }
     }
 
+    // Demo
     public static void main(String[] args) {
+
         Length l1 = new Length(1.0, LengthUnit.FEET);
         Length l2 = new Length(12.0, LengthUnit.INCHES);
 
-        System.out.println("1 ft == 12 inch -> " + l1.equals(l2));
+        Length l3 = new Length(1.0, LengthUnit.YARDS);
+        Length l4 = new Length(3.0, LengthUnit.FEET);
+
+        Length l5 = new Length(30.48, LengthUnit.CENTIMETERS);
+        Length l6 = new Length(1.0, LengthUnit.FEET);
+
+        System.out.println("1 ft == 12 inches -> " + l1.equals(l2));
+        System.out.println("1 yard == 3 feet -> " + l3.equals(l4));
+        System.out.println("30.48 cm == 1 foot -> " + l5.equals(l6));
     }
 }
