3fe6cfc95e74f4d21912b40dabfd3f019e52c9f0
Mutthuram03 <srmutthuram2007@gmail.com>
2026-04-27 09:39:46 +0530

    UC6: Added addition of lengths

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,75 +1,49 @@
 package com.apps.quantitymeasurement;
 
-import java.util.Objects;
-
 public class QuantityMeasurementApp {
 
-    // Enum for all supported units
     public enum LengthUnit {
         FEET(1.0),
         INCHES(1.0 / 12.0),
         YARDS(3.0),
         CENTIMETERS(0.0328084);
 
-        private final double toFeet;
+        private final double toFeetFactor;
 
-        LengthUnit(double toFeet) {
-            this.toFeet = toFeet;
+        LengthUnit(double toFeetFactor) {
+            this.toFeetFactor = toFeetFactor;
         }
 
         public double toFeet(double value) {
-            return value * toFeet;
+            return value * toFeetFactor;
+        }
+
+        public double fromFeet(double feet) {
+            return feet / toFeetFactor;
         }
     }
 
-    // Generic Length class
     public static class Length {
         private final double value;
         private final LengthUnit unit;
 
         public Length(double value, LengthUnit unit) {
-            if (unit == null) {
-                throw new IllegalArgumentException("Unit cannot be null");
-            }
             this.value = value;
             this.unit = unit;
         }
 
-        // Convert to base unit (Feet)
         private double toBase() {
             return unit.toFeet(value);
         }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null) return false;
-            if (getClass() != obj.getClass()) return false;
-
-            Length other = (Length) obj;
-            return Double.compare(this.toBase(), other.toBase()) == 0;
+        public Length add(Length other) {
+            double sumFeet = this.toBase() + other.toBase();
+            double result = unit.fromFeet(sumFeet);
+            return new Length(result, unit);
         }
 
-        @Override
-        public int hashCode() {
-            return Objects.hash(toBase());
+        public double getValue() {
+            return value;
         }
-    }
-
-    // Demo
-    public static void main(String[] args) {
-
-        Length l1 = new Length(1.0, LengthUnit.FEET);
-        Length l2 = new Length(12.0, LengthUnit.INCHES);
-
-        Length l3 = new Length(1.0, LengthUnit.YARDS);
-        Length l4 = new Length(3.0, LengthUnit.FEET);
-
-        Length l5 = new Length(30.48, LengthUnit.CENTIMETERS);
-        Length l6 = new Length(1.0, LengthUnit.FEET);
-
-        System.out.println("1 ft == 12 inches -> " + l1.equals(l2));
-        System.out.println("1 yard == 3 feet -> " + l3.equals(l4));
-        System.out.println("30.48 cm == 1 foot -> " + l5.equals(l6));
     }
 }
--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -9,49 +9,12 @@ import com.apps.quantitymeasurement.QuantityMeasurementApp.LengthUnit;
 public class QuantityMeasurementAppTest {
 
     @Test
-    public void testFeetEquality() {
-        Length l1 = new Length(1.0, LengthUnit.FEET);
-        Length l2 = new Length(1.0, LengthUnit.FEET);
-        assertTrue(l1.equals(l2));
-    }
-
-    @Test
-    public void testFeetToInches() {
+    public void testAdditionFeetAndInches() {
         Length l1 = new Length(1.0, LengthUnit.FEET);
         Length l2 = new Length(12.0, LengthUnit.INCHES);
-        assertTrue(l1.equals(l2));
-    }
 
-    @Test
-    public void testYardToFeet() {
-        Length l1 = new Length(1.0, LengthUnit.YARDS);
-        Length l2 = new Length(3.0, LengthUnit.FEET);
-        assertTrue(l1.equals(l2));
-    }
+        Length result = l1.add(l2);
 
-    @Test
-    public void testCmToFeet() {
-        Length l1 = new Length(30.48, LengthUnit.CENTIMETERS);
-        Length l2 = new Length(1.0, LengthUnit.FEET);
-        assertTrue(l1.equals(l2));
-    }
-
-    @Test
-    public void testDifferentValues() {
-        Length l1 = new Length(1.0, LengthUnit.FEET);
-        Length l2 = new Length(2.0, LengthUnit.FEET);
-        assertFalse(l1.equals(l2));
-    }
-
-    @Test
-    public void testNullComparison() {
-        Length l1 = new Length(1.0, LengthUnit.FEET);
-        assertFalse(l1.equals(null));
-    }
-
-    @Test
-    public void testSameReference() {
-        Length l1 = new Length(1.0, LengthUnit.FEET);
-        assertTrue(l1.equals(l1));
+        assertEquals(2.0, result.getValue(), 0.001);
     }
 }
